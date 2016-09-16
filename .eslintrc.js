de33fb6e354e127213d8648e86ac451d735651b1
Lukas Walisch <lukas.walisch@eguana.at>
2016-09-16 11:09:08 +0200

    added eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  extends: "airbnb/base",
+  env: {
+    'browser': true
+  },
+  "rules": {
+    "no-param-reassign": 0,
+    "max-len": 0,
+    "no-empty-label": 0,
+    "no-labels": 2,
+    "space-before-keywords": 0,
+    "keyword-spacing": 2,
+    "space-return-throw-case": 0,
+    "space-after-keywords": 0,
+    "space-before-function-paren": 0,
+    "padded-blocks": 0,
+    "no-shadow": 0,
+    "no-underscore-dangle": 0,
+    "no-loop-func": 0,
+    "import/no-extraneous-dependencies": 0
+
+  }
+};
