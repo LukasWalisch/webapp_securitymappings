19b006c609f8f6f505ca32fcdf03a2cd56a25576
Lukas Walisch <lukas.walisch@eguana.at>
2016-09-18 15:39:36 +0200

    added click events to network

--- app/controllers/mappings.js
+++ app/controllers/mappings.js
@@ -9,89 +9,123 @@ export default Ember.Controller.extend({
       vis
   */
 
+  /*
+  properties
+   */
+
+  nodeInfo: '-',
+  currentMappings: [],
+
+  /*
+  methods
+   */
+
+  /**
+   * fetches data from db and triggers renderNetwork
+   */
   init() {
     this._super();
+
     Ember.run.schedule('afterRender', this, function afterRender() {
-      this.renderNetwork();
+      // fetch data from db
+      const promiseArray = [];
+      promiseArray.push(this.get('store').findAll('tactic'));
+      promiseArray.push(this.get('store').findAll('pattern'));
+      promiseArray.push(this.get('store').findAll('mapping'));
+
+      Ember.RSVP.all(promiseArray).then(() => {
+
+        // when db fetches finished, render Network
+        this.renderNetwork();
+      });
     });
+
   },
 
+  /**
+   * renders the Network of tactics and patterns with options
+   * and registers on click listeners for nodes
+   */
   renderNetwork() {
 
-    let tactics;
-    let patterns;
-    let mappings;
+    const tactics = this.convertToArray(this.store.peekAll('tactic'));
+    const patterns = this.convertToArray(this.store.peekAll('pattern'));
+    const mappings = this.convertToArray(this.store.peekAll('mapping'));
 
-    const promiseArray = [];
+    const dynamicData = dataConverter.dataToDataset(tactics, patterns, mappings);
 
-    promiseArray.push(this.get('store').findAll('tactic'));
-    promiseArray.push(this.get('store').findAll('pattern'));
-    promiseArray.push(this.get('store').findAll('mapping'));
-
-    Ember.RSVP.all(promiseArray).then((results) => {
-
-      tactics = this.convertToArray(results[0]);
-      patterns = this.convertToArray(results[1]);
-      mappings = this.convertToArray(results[2]);
-
-      const dynamicData = dataConverter.dataToDataset(tactics, patterns, mappings);
-
-      const options = {
-        height: '400px',
-        width: '100%',
-        physics: {
-          enabled: false,
+    const options = {
+      height: '400px',
+      width: '100%',
+      physics: {
+        enabled: false,
+      },
+      manipulation: {
+        enabled: false,
+      },
+      layout: {
+        hierarchical: {
+          enabled: true,
+          sortMethod: 'directed',
         },
-        manipulation: {
-          enabled: false,
-        },
-        layout: {
-          hierarchical: {
-            enabled: true,
-            sortMethod: 'directed',
-          },
-        },
-        interaction: {
-          dragNodes: false,
-        },
+      },
+      interaction: {
+        dragNodes: false,
+      },
 
-      };
+    };
 
-      const container = document.getElementById('visual-container');
-      const network = new vis.Network(container, dynamicData, options);
+    const container = document.getElementById('visual-container');
+    const network = new vis.Network(container, dynamicData, options);
 
-      network.on('click', (event) => {
+    network.on('click', (event) => {
 
-        // focus on a node should only be executed when a node is clicked
-        if (event.nodes.length > 0) {
-          network.focus(event.nodes[0], {
-            scale: 1.0,
-            offset: { x: 0, y: -100 },
-            animation: true,
-          });
-        }
+      // displays the info of the current selection in the info section
+      this.displayNodeData(event);
 
-      });
+      if (event.nodes.length > 0) {
+        // when node is clicked, the node is moving in center of window
+        network.focus(event.nodes[0], {
+          scale: 1.0,
+          offset: { x: 0, y: 0 },
+          animation: true,
+        });
+      }
 
     });
-
-    // const nodes = [
-    //   { id: 1, label: 'a' },
-    //   { id: 2, label: 'b' },
-    //   { id: 3, label: 'c' },
-    // ];
-    // const edges = [
-    //   { from: 1, to: 2 },
-    //   { from: 1, to: 3 },
-    // ];
-
-    // const data = {
-    //   nodes,
-    //   edges,
-    // };
-
   },
 
+  /**
+   * changes the state of the property nodeInfo and currentMappings
+   * to the info of a clicked node
+   * @param  {vis event} event
+   */
+  displayNodeData(event) {
+
+    if (event.nodes.length > 0) {
+      const nodeId = event.nodes[0];
+      let node = this.store.peekRecord('tactic', nodeId);
+
+      // if node exists --> its a tactic
+      if (!node) {
+
+        // its a pattern
+        node = this.store.peekRecord('pattern', nodeId);
+      }
+      this.set('nodeInfo', node.toJSON().info);
+      this.set('currentMappings', node.get('mappingIds'));
+    } else {
+      this.set('nodeInfo', '-');
+      this.set('currentMappings', []);
+    }
+  },
+
+
+  /**
+   * converts Array from Ember Store to Array
+   * @param  {EmberArray} emberArray
+   * @return {Array}
+   */
   convertToArray(emberArray) {
     return emberArray.map((entry) => {
       const helper = entry.toJSON();
--- app/utils/dataConverter.js
+++ app/utils/dataConverter.js
@@ -5,28 +5,39 @@
 
 const dataConverter = {
 
+  /**
+   * converts normal arrays of tacitcs, patterns and mapping to a vis dataset
+   * @return {Object} Dataset, Example:
+    // const nodes = [
+    //   { id: 1, label: 'a' },
+    //   { id: 2, label: 'b' },
+    //   { id: 3, label: 'c' },
+    // ];
+    // const edges = [
+    //   { from: 1, to: 2 },
+    //   { from: 1, to: 3 },
+    // ];
+    // const data = {
+    //   nodes,
+    //   edges,
+    // };
+   */
   dataToDataset(tactics, patterns, mappings) {
 
     const nodes = [];
     const edges = [];
-
     for (let tactic of tactics) {
       nodes.push({ id: tactic.id, label: tactic.name, color: 'green' });
-
       for (let childTacticId of tactic.childTacticIds) {
         edges.push({ from: tactic.id, to: childTacticId });
       }
-
     }
-
     for (let pattern of patterns) {
       nodes.push({ id: pattern.id, label: pattern.name, color: 'red' });
     }
-
     for (let mapping of mappings) {
       edges.push({ from: mapping.tacticId, to: mapping.patternId });
     }
-
     return { nodes, edges };
   },
 
